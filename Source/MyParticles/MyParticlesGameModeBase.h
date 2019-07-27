9dd92e32361cbfdf86a92b9522257bc474e9f048
lilieming <lilieminglook@gmail.com>
2019-07-27 08:28:39 +0800

    init

---
+++ Source/MyParticles/MyParticlesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyParticlesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPARTICLES_API AMyParticlesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
